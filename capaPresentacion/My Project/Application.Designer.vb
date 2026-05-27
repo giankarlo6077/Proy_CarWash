2f7f8655cc5ccf102c55602cea3c73c94f69315c
Edgar Bernabe <tu_correo@ejemplo.com>
2026-05-27 16:26:10 -0500

    arreglado que siempre abría el mismo frm

--- capaPresentacion/My Project/Application.Designer.vb
+++ capaPresentacion/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.capaPresentacion.jdGestionarCitas
+            Me.MainForm = Global.capaPresentacion.FrmMenuPrincipalModificado
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
